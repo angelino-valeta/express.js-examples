44a94d960b933deb7b0c79c934157938268631a8
Angelino Valeta <angelinovaleta@gmail.com>
2024-01-02 23:32:43 +0100

    config in

--- route-separation/index.js
+++ route-separation/index.js
@@ -6,4 +6,13 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 
+module.exports = app
+
+// Config
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(logger('dev'))
+
 app.listen(3000, () => { console.log('Express started on port 3000')})
